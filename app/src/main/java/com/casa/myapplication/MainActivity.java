a2740e16c507348cd805fe120d053fff452da8d9
max <ansegundo1@gmail.com>
2017-06-13 18:51:39 +0200

    Initial commit

---
+++ app/src/main/java/com/casa/myapplication/MainActivity.java
@@ -0,0 +1,92 @@
+package com.casa.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText mMailField, mPassField;
+    private Button mButtonLog;
+
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        ActionBar actionBar = getSupportActionBar();
+        if(actionBar != null){
+            actionBar.hide();
+        }
+
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+
+        mMailField = (EditText) findViewById(R.id.mailField);
+        mPassField = (EditText) findViewById(R.id.passField);
+        mButtonLog = (Button) findViewById(R.id.buttonLog);
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                if(firebaseAuth.getCurrentUser() != null){ //el usuario está logeado ya
+                    startActivity(new Intent(MainActivity.this, MenuActivity.class));
+                }
+            }
+        };
+
+
+        mButtonLog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startLogin();
+            }
+        });
+
+    }
+
+    private void startLogin(){
+
+        String email = mMailField.getText().toString().trim();
+        String pass = mPassField.getText().toString().trim();
+
+        if(TextUtils.isEmpty(email) || TextUtils.isEmpty(pass)/*probar --> email.isEmpty() || pass.isEmpty()*/){
+
+            Toast.makeText(MainActivity.this, "Campos vacios", Toast.LENGTH_LONG).show();
+
+        }else{
+
+            mFirebaseAuth.signInWithEmailAndPassword(email, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(!task.isSuccessful()){
+                        Toast.makeText(MainActivity.this, "Error usuario y/o contraseña", Toast.LENGTH_LONG).show();
+                    }
+                }
+            });
+
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mFirebaseAuth.addAuthStateListener(mAuthListener);
+    }
+}
---
+++ app/src/main/java/com/casa/myapplication/NavMenu/Menu1.java
@@ -0,0 +1,10 @@
+package com.casa.myapplication.NavMenu;
+
+//import android.app.Fragment;
+import android.support.v4.app.Fragment;
+
+
+public class Menu1 extends Fragment {
+
+
+}
