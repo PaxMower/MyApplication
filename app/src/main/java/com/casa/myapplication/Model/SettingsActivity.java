0c517bac8e6fad6ce8895138e1dc5affe520935c
max <ansegundo1@gmail.com>
2018-06-08 17:02:19 +0200

    POR HACER ------------------------------------ Cambiar botones MenuActivity Tratar la excepción sobre selecciónar fechas y horas anteriores en las descargas a las fechas y horas generadas en las cargas de contenedores (PROBAR RECURSIVIDAD A OTRO MÉTODO) Añadir SharedPreferences para guardar datos del formulario localmente Botón back para salir de la app Obtener latitud y longitud de la posición actual para guardar un destino en Firebase Poblar BBDD de ciudades con sus respectivos kilómetros Autocompletar los campos correspondientes a la empresa cuando sea seleccionada una Calcular precio según los kilómetros Sale onPause cuando entro a nuevo viaje. Arreglar!!!!!! processDialog cuando cambies valores de perfil Añadir botón para guardar el formulario (Para guardarlo localmente). Añadir botón de foto en el ActionBar para subir fotos al servidor Ver como rellenar el fondo de MenuActivity (para que no sea un fondo blanco solamente) Añadir efectos a los botones (quizá esta opción sea eliminada) RetrieveData on ExpandableListView -- FALTA INFORMACIÓN Poner buscador para los markers de GoogleMaps (una vez elegido un destino, se recuperará las coordenadas de ese destino para ponerlas en el mapa) Añadir tiempo estimado de viaje Añadir tiempo estimado de cambio de aceite, ruedas, etc...
    EN PROCESO------------------------------------
    Layout perfgil de usuario/ajustes
    Firebase para el perfil de usuario (SharedPreferences no)
    Tratar excepción fecha automática
    
    HECHO------------------------------------------
    processDialog en repostar
    Añadir opción de repostar(Falta añadirlo en el mainPage y hacer que envie datos)
    Cambiar formato fecha para firebase (poner YYYY/MM/DD) :  Se ha cambiado a valores dentro de la orden
    processDialog en enviar order
    Quitar campo destino
    Mover botón del mapa a un botón de chincheta junto a la dirección
    Añadir alertDialog para el botón Salir (salir de la cuenta actual, no de la aplicación)
    Añadir TextArea de observaciones
    Añadir los AlertDialog en todos los DatePicker y TimePicker
    Eliminado el hecho de hacer un formulario en varias fases
    Ya se envian datos a Firebase.
    GoogleMaps desde opción "nueva orden"
    Zoom en el GoogleMaps
    Botones para cambiar terreno entre satelite y normal
    Añadir permisos para obtener la localización GPS
    Poner el botón de localización en el mapa + movimiento de cámara
    botón para obtener la hora
    Acabar formulario
    Añadir datepicker y timepicker para obtener fecha y hora cuando se pulsa un botón (hecho en formato 24 horas)

--- app/src/main/java/com/casa/myapplication/Model/SettingsActivity.java
+++ app/src/main/java/com/casa/myapplication/Model/SettingsActivity.java
@@ -1,12 +1,16 @@
 package com.casa.myapplication.Model;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.casa.myapplication.R;
@@ -14,25 +18,58 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 
+import de.hdodenhof.circleimageview.CircleImageView;
+
 
 public class SettingsActivity extends AppCompatActivity{
 
-    private TextView mTextView;
+    private CircleImageView mImage;
+    private TextView mTruckId, mTruckNum, mPlatformId;
+    private Button mChangeTruckId, mChangeTruckNum, mChangePlatformId;
+
+
     private FirebaseAuth mFirebaseAuth = FirebaseAuth.getInstance();
     private FirebaseUser mFirebaseUser = mFirebaseAuth.getCurrentUser();
     private DatabaseReference mDatabase;
-    FirebaseUser current_user = FirebaseAuth.getInstance().getCurrentUser();
-    String uid = current_user.getUid();
+    private FirebaseUser current_user = FirebaseAuth.getInstance().getCurrentUser();
+    private String uid = current_user.getUid();
 
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
-        mTextView = (TextView) findViewById(R.id.textViewSettings);
+        mImage = (CircleImageView) findViewById(R.id.profile_image);
+        mTruckId = (TextView) findViewById(R.id.truck_id_settings);
+        mTruckNum = (TextView) findViewById(R.id.truck_num_settings);
+        mPlatformId = (TextView) findViewById(R.id.platform_id_settings);
+        mChangeTruckId = (Button) findViewById(R.id.edit_truck_num);
+        mChangeTruckNum = (Button) findViewById(R.id.edit_truck_id);
+        mChangePlatformId = (Button) findViewById(R.id.edit_platform_id);
 
         getSupportActionBar().setDisplayShowHomeEnabled(true); //Establece si incluir la aplicación home en la toolbar
         getSupportActionBar().setDisplayHomeAsUpEnabled(true); //Establece si el home se mopstrará como un UP
+
+        LoadProfile();
+        ChangeSettings();
+    }
+
+    private void LoadProfile() {
+
+    }
+
+    private void ChangeSettings() {
+        mChangeTruckId.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                AlertDialog.Builder alertBuild = new AlertDialog.Builder(SettingsActivity.this);
+                alertBuild.setMessage("Inserte nuevo número de camión")
+                    .setCancelable(false)
+                    .set
+            }
+        });
     }
 
     @Override
