8fad095f5fbf6f6dd5c13cb6c0b31751d2c7bbff
max <ansegundo1@gmail.com>
2018-06-11 19:07:27 +0200

    POR HACER ------------------------------------ Cambiar botones MenuActivity Tratar la excepción sobre selecciónar fechas y horas anteriores en las descargas a las fechas y horas generadas en las cargas de contenedores (PROBAR RECURSIVIDAD A OTRO MÉTODO) Añadir SharedPreferences para guardar datos del formulario localmente Botón back para salir de la app Obtener latitud y longitud de la posición actual para guardar un destino en Firebase Poblar BBDD de ciudades con sus respectivos kilómetros Autocompletar los campos correspondientes a la empresa cuando sea seleccionada una Calcular precio según los kilómetros Sale onPause cuando entro a nuevo viaje. Arreglar!!!!!! processDialog cuando cambies valores de perfil Añadir botón para guardar el formulario (Para guardarlo localmente). Añadir botón de foto en el ActionBar para subir fotos al servidor Ver como rellenar el fondo de MenuActivity (para que no sea un fondo blanco solamente) Añadir efectos a los botones (quizá esta opción sea eliminada) RetrieveData on ExpandableListView -- FALTA INFORMACIÓN Poner buscador para los markers de GoogleMaps (una vez elegido un destino, se recuperará las coordenadas de ese destino para ponerlas en el mapa) Añadir tiempo estimado de viaje Añadir tiempo estimado de cambio de aceite, ruedas, etc...
    EN PROCESO------------------------------------
    Layout perfil de usuario/ajustes
    Firebase para el perfil de usuario (SharedPreferences no)
    Tratar excepción fecha automática
    
    HECHO------------------------------------------
    processDialog en repostar
    Añadir opción de repostar(Falta añadirlo en el mainPage y hacer que envie datos)
    Cambiar formato fecha para firebase (poner YYYY/MM/DD) :  Se ha cambiado a valores dentro de la orden
    processDialog en enviar order
    Quitar campo destino
    Mover botón del mapa a un botón de chincheta junto a la dirección
    Añadir alertDialog para el botón Salir (salir de la cuenta actual, no de la aplicación)
    Añadir TextArea de observaciones
    Añadir los AlertDialog en todos los DatePicker y TimePicker
    Eliminado el hecho de hacer un formulario en varias fases
    Ya se envian datos a Firebase.
    GoogleMaps desde opción "nueva orden"
    Zoom en el GoogleMaps
    Botones para cambiar terreno entre satelite y normal
    Añadir permisos para obtener la localización GPS
    Poner el botón de localización en el mapa + movimiento de cámara
    botón para obtener la hora
    Acabar formulario
    Añadir datepicker y timepicker para obtener fecha y hora cuando se pulsa un botón (hecho en formato 24 horas)

---
+++ app/src/main/java/com/casa/myapplication/Model/ChangeSettingsActivity.java
@@ -0,0 +1,101 @@
+package com.casa.myapplication.Model;
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.casa.myapplication.Logic.User;
+import com.casa.myapplication.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class ChangeSettingsActivity extends AppCompatActivity {
+
+    private TextView mNewTruckId, mNewTruckNum, mNewPlatformId;
+    private Button mSaveNewData;
+    User newUser = new User();
+    private ProgressDialog mProgressLoad;
+    private DatabaseReference mDatabase;
+    FirebaseUser current_user = FirebaseAuth.getInstance().getCurrentUser();
+    String uid = current_user.getUid();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change_settings);
+        mNewTruckId = (TextView)findViewById(R.id.newTruckId);
+        mNewTruckNum = (TextView)findViewById(R.id.newTruckNum);
+        mNewPlatformId = (TextView)findViewById(R.id.newPlatformId);
+        mSaveNewData = (Button) findViewById(R.id.saveNewData);
+
+        //Add back buttons on toolbar
+        getSupportActionBar().setDisplayShowHomeEnabled(true); //Establece si incluir la aplicación home en la toolbar
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true); //Establece si el home se mopstrará como un UP
+
+        SaveNewData();
+    }
+
+    private void SaveNewData() {
+
+        mDatabase = FirebaseDatabase.getInstance().getReference().child("Users").child(uid).child("Settings");
+
+        mSaveNewData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                newUser.setTruckIdSettings(mNewTruckId.getText().toString());
+                newUser.setTruckNumSettings(mNewTruckNum.getText().toString());
+                newUser.setPlatformIdSettings(mNewPlatformId.getText().toString());
+
+                mProgressLoad = new ProgressDialog(ChangeSettingsActivity.this);
+                mProgressLoad.setTitle("Guardando");
+                mProgressLoad.setMessage("Guardando datos, por favor espere");
+                mProgressLoad.setCanceledOnTouchOutside(false);
+                mProgressLoad.show();
+
+                mDatabase.child("PlatformId").updateChildren();
+
+
+                /*
+                mDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        mDatabase.child("PlatformId").setValue(newUser.getPlatformIdSettings());
+                        mDatabase.child("TruckId").setValue(newUser.getTruckIdSettings());
+                        mDatabase.child("TruckNumber").setValue(newUser.getTruckNumSettings());
+                        mProgressLoad.dismiss();
+
+                        Intent goToMainPage = new Intent(ChangeSettingsActivity.this, SettingsActivity.class);
+                        goToMainPage.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                        startActivity(goToMainPage);
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+                        Toast.makeText(ChangeSettingsActivity.this, "Error al cargar los datos", Toast.LENGTH_LONG).show();
+                    }
+                });*/
+
+            }
+        });
+    }
+
+    //Close actual activity when back button is selected
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case android.R.id.home:
+                this.finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
