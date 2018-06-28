ec0326402420bb7557aa8f4c7312bc4155882021
max <ansegundo1@gmail.com>
2018-06-28 19:52:10 +0200

    POR HACER ------------------------------------
    Obtener latitud y longitud de la posición actual para guardar un destino en Firebase
    Cambiar botones MenuActivity
    Tratar la excepción sobre selecciónar fechas y horas anteriores en las descargas a las fechas y horas generadas en las cargas de contenedores (PROBAR RECURSIVIDAD A OTRO MÉTODO)
    Botón back para salir de la app
    Poblar BBDD de ciudades con sus respectivos kilómetros
    Autocompletar los campos correspondientes a la empresa cuando sea seleccionada una
    Calcular precio según los kilómetros
    Sale onPause cuando entro a nuevo viaje. Arreglar!!!!!!
    processDialog cuando cambies valores de perfil
    Añadir botón para guardar el formulario (Para guardarlo localmente).
    Añadir botón de foto en el ActionBar para subir fotos al servidor
    Ver como rellenar el fondo de MenuActivity (para que no sea un fondo blanco solamente)
    Añadir efectos a los botones (quizá esta opción sea eliminada)
    RetrieveData on ExpandableListView -- FALTA INFORMACIÓN
    Poner buscador para los markers de GoogleMaps (una vez elegido un destino, se recuperará las coordenadas de ese destino para ponerlas en el mapa)
    Añadir tiempo estimado de viaje
    Añadir tiempo estimado de cambio de aceite, ruedas, etc...
    Cambiar apariencia menú ajustes
    En ajustes, avitar poner datos erróneos (que la matricula tenga 4 números y 3 letras a continuación, por ejemplo)
    
    EN PROCESO------------------------------------
    Cargar lista de datos para ver las órdenes -- Ya salen los meses, faltan los datos de cada orden
    Cargar datos de usuario automáticamente en la nueva orden
    Añadir SharedPreferences para guardar datos del formulario localmente
    Eliminar SharedPreferences cuando acabemos una orden de trabajo
    Buscar info sobre guarda datos offline (método de Firebase)
    Tratar excepción fecha automática
    
    HECHO------------------------------------------
    processDialog en repostar
    Añadir opción de repostar(Falta añadirlo en el mainPage y hacer que envie datos)
    Cambiar formato fecha para firebase (poner YYYY/MM/DD) :  Se ha cambiado a valores dentro de la orden
    processDialog en enviar order
    Quitar campo destino
    Mover botón del mapa a un botón de chincheta junto a la dirección
    Añadir alertDialog para el botón Salir (salir de la cuenta actual, no de la aplicación)
    Añadir TextArea de observaciones
    Añadir los AlertDialog en todos los DatePicker y TimePicker
    Eliminado el hecho de hacer un formulario en varias fases
    Ya se envian datos a Firebase.
    GoogleMaps desde opción "nueva orden"
    Zoom en el GoogleMaps
    Botones para cambiar terreno entre satelite y normal
    Añadir permisos para obtener la localización GPS
    Poner el botón de localización en el mapa + movimiento de cámara
    botón para obtener la hora
    Acabar formulario
    Añadir datepicker y timepicker para obtener fecha y hora cuando se pulsa un botón (hecho en formato 24 horas)

--- app/src/main/java/com/casa/myapplication/Adapter/CustomExpandableListView.java
+++ app/src/main/java/com/casa/myapplication/Adapter/CustomExpandableListView.java
@@ -7,6 +7,7 @@ import android.view.ViewGroup;
 import android.widget.BaseExpandableListAdapter;
 import android.widget.TextView;
 
+import com.casa.myapplication.Logic.Order;
 import com.casa.myapplication.R;
 
 import java.util.HashMap;
@@ -16,13 +17,14 @@ public class CustomExpandableListView extends BaseExpandableListAdapter {
 
     Context context;
     List<String> listHeader;
-    HashMap<String, List<String>> listChild;
+    HashMap<String, List<Order>> listChild;
 
-    public CustomExpandableListView(Context context, List<String> listHeader, HashMap<String, List<String>> listChild) {
+    public CustomExpandableListView(Context context, List<String> listHeader, HashMap<String, List<Order>> listChild) {
         this.context = context;
         this.listHeader = listHeader;
         this.listChild = listChild;
     }
+
 
     @Override
     public int getGroupCount() {
@@ -65,17 +67,21 @@ public class CustomExpandableListView extends BaseExpandableListAdapter {
         LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         convertView = inflater.inflate(R.layout.item_parent, null);
         TextView textHeader = (TextView) convertView.findViewById(R.id.headerText);
+        TextView textHeader2 = (TextView) convertView.findViewById(R.id.headerText2);
         textHeader.setText(header);
+        textHeader2.setText(header);
         return convertView;
     }
 
     @Override
     public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
-        String item = (String) getChild(groupPosition, childPosition);
+        Order item = (Order) getChild(groupPosition, childPosition);
         LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         convertView = inflater.inflate(R.layout.item_child, null);
-        TextView txtItem = (TextView) convertView.findViewById(R.id.dayRetrieve);
-        txtItem.setText(item);
+        TextView day = (TextView) convertView.findViewById(R.id.dayRetrieve);
+        TextView hour = (TextView) convertView.findViewById(R.id.hourRetrieve);
+        day.setText(item.getDay());
+        hour.setText(item.getHour());
         return convertView;
     }
 
