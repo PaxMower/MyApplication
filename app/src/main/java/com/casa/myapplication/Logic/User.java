8cee7d9723cf5a6f68bd7b5f94650465fe80c529
max <ansegundo1@gmail.com>
2018-06-29 18:55:58 +0200

    POR HACER ------------------------------------
    Obtener latitud y longitud de la posición actual para guardar un destino en Firebase
    Cambiar botones MenuActivity
    Tratar la excepción sobre selecciónar fechas y horas anteriores en las descargas a las fechas y horas generadas en las cargas de contenedores (PROBAR RECURSIVIDAD A OTRO MÉTODO)
    Botón back para salir de la app
    Poblar BBDD de ciudades con sus respectivos kilómetros
    Autocompletar los campos correspondientes a la empresa cuando sea seleccionada una
    Calcular precio según los kilómetros
    Sale onPause cuando entro a nuevo viaje. Arreglar!!!!!!
    processDialog cuando cambies valores de perfil
    Añadir botón para guardar el formulario (Para guardarlo localmente).
    Añadir botón de foto en el ActionBar para subir fotos al servidor
    Ver como rellenar el fondo de MenuActivity (para que no sea un fondo blanco solamente)
    Añadir efectos a los botones (quizá esta opción sea eliminada)
    RetrieveData on ExpandableListView -- FALTA INFORMACIÓN
    Poner buscador para los markers de GoogleMaps (una vez elegido un destino, se recuperará las coordenadas de ese destino para ponerlas en el mapa)
    Añadir tiempo estimado de viaje
    Añadir tiempo estimado de cambio de aceite, ruedas, etc...
    Cambiar apariencia menú ajustes
    En ajustes, avitar poner datos erróneos (que la matricula tenga 4 números y 3 letras a continuación, por ejemplo)
    
    EN PROCESO------------------------------------
    Modificar Loyout datos de lista expandible -- Ya muestra los datos
    Cargar datos de usuario automáticamente en la nueva orden
    Añadir SharedPreferences para guardar datos del formulario localmente
    Eliminar SharedPreferences cuando acabemos una orden de trabajo
    Buscar info sobre guarda datos offline (método de Firebase)
    Tratar excepción fecha automática
    
    HECHO------------------------------------------
    processDialog en repostar
    Añadir opción de repostar(Falta añadirlo en el mainPage y hacer que envie datos)
    Cambiar formato fecha para firebase (poner YYYY/MM/DD) :  Se ha cambiado a valores dentro de la orden
    processDialog en enviar order
    Quitar campo destino
    Mover botón del mapa a un botón de chincheta junto a la dirección
    Añadir alertDialog para el botón Salir (salir de la cuenta actual, no de la aplicación)
    Añadir TextArea de observaciones
    Añadir los AlertDialog en todos los DatePicker y TimePicker
    Eliminado el hecho de hacer un formulario en varias fases
    Ya se envian datos a Firebase.
    GoogleMaps desde opción "nueva orden"
    Zoom en el GoogleMaps
    Botones para cambiar terreno entre satelite y normal
    Añadir permisos para obtener la localización GPS
    Poner el botón de localización en el mapa + movimiento de cámara
    botón para obtener la hora
    Acabar formulario
    Añadir datepicker y timepicker para obtener fecha y hora cuando se pulsa un botón (hecho en formato 24 horas)

--- app/src/main/java/com/casa/myapplication/Logic/User.java
+++ app/src/main/java/com/casa/myapplication/Logic/User.java
@@ -4,7 +4,49 @@ package com.casa.myapplication.Logic;
 
 public class User {
 
+    private String employeeNameSettings, platformIdSettings, truckIdSettings, truckNumSettings;
 
+    public User(String employeeNameSettings, String platformIdSettings, String truckIdSettings, String truckNumSettings) {
 
+        this.employeeNameSettings = employeeNameSettings;
+        this.platformIdSettings = platformIdSettings;
+        this.truckIdSettings = truckIdSettings;
+        this.truckNumSettings = truckNumSettings;
+    }
 
+    public User (){
+        
+    }
+
+    public String getEmployeeNameSettings() {
+        return employeeNameSettings;
+    }
+
+    public void setEmployeeNameSettings(String employeeNameSettings) {
+        this.employeeNameSettings = employeeNameSettings;
+    }
+
+    public String getPlatformIdSettings() {
+        return platformIdSettings;
+    }
+
+    public void setPlatformIdSettings(String platformIdSettings) {
+        this.platformIdSettings = platformIdSettings;
+    }
+
+    public String getTruckIdSettings() {
+        return truckIdSettings;
+    }
+
+    public void setTruckIdSettings(String truckIdSettings) {
+        this.truckIdSettings = truckIdSettings;
+    }
+
+    public String getTruckNumSettings() {
+        return truckNumSettings;
+    }
+
+    public void setTruckNumSettings(String truckNumSettings) {
+        this.truckNumSettings = truckNumSettings;
+    }
 }
