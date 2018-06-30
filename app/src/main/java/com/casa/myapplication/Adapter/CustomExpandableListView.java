2b02f64164ec36b4c8370c970ca241f604902093
max <ansegundo1@gmail.com>
2018-06-30 22:04:55 +0200

    POR HACER ------------------------------------
    Obtener latitud y longitud de la posición actual para guardar un destino en Firebase
    Cambiar botones MenuActivity
    Tratar la excepción sobre selecciónar fechas y horas anteriores en las descargas a las fechas y horas generadas en las cargas de contenedores (PROBAR RECURSIVIDAD A OTRO MÉTODO)
    Botón back para salir de la app
    Poblar BBDD de ciudades con sus respectivos kilómetros
    Autocompletar los campos correspondientes a la empresa cuando sea seleccionada una
    Calcular precio según los kilómetros
    Sale onPause cuando entro a nuevo viaje. Arreglar!!!!!!
    processDialog cuando cambies valores de perfil
    Añadir botón para guardar el formulario (Para guardarlo localmente).
    Añadir botón de foto en el ActionBar para subir fotos al servidor
    Ver como rellenar el fondo de MenuActivity (para que no sea un fondo blanco solamente)
    Añadir efectos a los botones (quizá esta opción sea eliminada)
    RetrieveData on ExpandableListView -- FALTA INFORMACIÓN
    Poner buscador para los markers de GoogleMaps (una vez elegido un destino, se recuperará las coordenadas de ese destino para ponerlas en el mapa)
    Añadir tiempo estimado de viaje
    Añadir tiempo estimado de cambio de aceite, ruedas, etc...
    Cambiar apariencia menú ajustes
    En ajustes, avitar poner datos erróneos (que la matricula tenga 4 números y 3 letras a continuación, por ejemplo)
    
    EN PROCESO------------------------------------
    Mostrar datos
    Cargar datos de usuario automáticamente en la nueva orden
    Añadir SharedPreferences para guardar datos del formulario localmente
    Eliminar SharedPreferences cuando acabemos una orden de trabajo
    Buscar info sobre guarda datos offline (método de Firebase)
    Tratar excepción fecha automática
    
    HECHO------------------------------------------
    Modificar Layout de child list
    processDialog en repostar
    Añadir opción de repostar(Falta añadirlo en el mainPage y hacer que envie datos)
    Cambiar formato fecha para firebase (poner YYYY/MM/DD) :  Se ha cambiado a valores dentro de la orden
    processDialog en enviar order
    Quitar campo destino
    Mover botón del mapa a un botón de chincheta junto a la dirección
    Añadir alertDialog para el botón Salir (salir de la cuenta actual, no de la aplicación)
    Añadir TextArea de observaciones
    Añadir los AlertDialog en todos los DatePicker y TimePicker
    Eliminado el hecho de hacer un formulario en varias fases
    Ya se envian datos a Firebase.
    GoogleMaps desde opción "nueva orden"
    Zoom en el GoogleMaps
    Botones para cambiar terreno entre satelite y normal
    Añadir permisos para obtener la localización GPS
    Poner el botón de localización en el mapa + movimiento de cámara
    botón para obtener la hora
    Acabar formulario
    Añadir datepicker y timepicker para obtener fecha y hora cuando se pulsa un botón (hecho en formato 24 horas)

--- app/src/main/java/com/casa/myapplication/Adapter/CustomExpandableListView.java
+++ app/src/main/java/com/casa/myapplication/Adapter/CustomExpandableListView.java
@@ -78,10 +78,47 @@ public class CustomExpandableListView extends BaseExpandableListAdapter {
         Order item = (Order) getChild(groupPosition, childPosition);
         LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         convertView = inflater.inflate(R.layout.item_child, null);
-        TextView day = (TextView) convertView.findViewById(R.id.dayRetrieve);
-        TextView hour = (TextView) convertView.findViewById(R.id.hourRetrieve);
-        day.setText(item.getDay());
-        hour.setText(item.getHour());
+
+        TextView date = (TextView) convertView.findViewById(R.id.date);
+        TextView truckId = (TextView) convertView.findViewById(R.id.truckIdRetrieve);
+        TextView truckNum = (TextView) convertView.findViewById(R.id.truckNumRetrieve);
+        TextView platformId = (TextView) convertView.findViewById(R.id.platformIdRetrieve);
+        TextView containerId = (TextView) convertView.findViewById(R.id.containerIdRetrieve);
+        TextView charger = (TextView) convertView.findViewById(R.id.chargerRetrieve);
+        TextView client = (TextView) convertView.findViewById(R.id.clientRetrieve);
+        TextView address = (TextView) convertView.findViewById(R.id.addressRetrieve);
+        TextView apertureHour = (TextView) convertView.findViewById(R.id.apertureHourRetrieve);
+        TextView phone = (TextView) convertView.findViewById(R.id.phoneRetrieve);
+        TextView arrivalHour = (TextView) convertView.findViewById(R.id.arrivalHourRetrieve);
+        TextView departureHour = (TextView) convertView.findViewById(R.id.departureHourRetrieve);
+        TextView ccd= (TextView) convertView.findViewById(R.id.containerChargeDayRetrieve);
+        TextView cch = (TextView) convertView.findViewById(R.id.containerChargeHourRetrieve);
+        TextView cdd = (TextView) convertView.findViewById(R.id.containerDischargeDayRetrieve);
+        TextView cdh = (TextView) convertView.findViewById(R.id.containerDischargeHourRetrieve);
+        TextView observations = (TextView) convertView.findViewById(R.id.observationsRetrieve);
+
+        //*************************************/
+        //COMPLETAR
+        //*************************************/
+        date.setText(item.getDay());
+        truckId.setText(item.getDay());
+        truckNum.setText(item.getDay());
+        platformId.setText(item.getDay());
+        containerId.setText(item.getDay());
+        charger.setText(item.getDay());
+        client.setText(item.getDay());
+        address.setText(item.getDay());
+        apertureHour.setText(item.getDay());
+        phone.setText(item.getDay());
+        arrivalHour.setText(item.getDay());
+        departureHour.setText(item.getDay());
+        ccd.setText(item.getDay());
+        cch.setText(item.getDay());
+        cdd.setText(item.getDay());
+        cdh.setText(item.getDay());
+        observations.setText(item.getDay());
+
+
         return convertView;
     }
 
